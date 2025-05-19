3cd02d0bfaa54d7243fbb1e05690be95416901d1
Akmal Fazili <92304771+Akmalfazili@users.noreply.github.com>
2025-05-20 00:14:07 +0800

    20/5/25 1st push

---
+++ worldCities.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using worldCities.Server.Data.Models;
+
+namespace worldCities.Server.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<City> Cities => Set<City>();
+        public DbSet<Country> Country => Set<Country>();
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.ApplyConfigurationsFromAssembly(
+        //        typeof(ApplicationDbContext).Assembly );
+        //}
+    }
+}
